fcfe2dc91f6769fa9639f91394ee57c7b6a13df2
ccashley12 <ccashley12@gmail.com>
2025-01-11 12:01:56 -0500

    commit title

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -5,7 +5,7 @@ const calendar = google.calendar("v3");
 const SCOPES = ["https://www.googleapis.com/auth/calendar.events.public.readonly"];
 const { CLIENT_SECRET, CLIENT_ID, CALENDAR_ID } = process.env;
 const redirect_uris = [
-    "https://meet-phi-livid.vercel.app/"
+    "https://vercel.com/chris-projects-b04bbcb0/meet"
 ];
 
 const oAuth2Client = new google.auth.OAuth2(
@@ -15,6 +15,7 @@ const oAuth2Client = new google.auth.OAuth2(
 );
 
 module.exports.getAuthURL = async () => {
+
     const authUrl = oAuth2Client.generateAuthUrl({
         access_type: "offline",
         scope: SCOPES,
@@ -59,4 +60,45 @@ module.exports.getAccessToken = async (event) => {
                 body: JSON.stringify(error),
             };
         });
+};
+
+module.exports.getCalendarEvents = async (event) => {
+    const access_token = decodeURIComponent(`${event.pathParameters.access_token}`);
+
+    oAuth2Client.setCredentials({ access_token });
+
+    return new Promise((resolve, reject) => {
+        calendar.events.list(
+            {
+                calendarId: CALENDAR_ID,
+                auth: oAuth2Client,
+                timeMin: new Date().toISOString(),
+                singleEvents: true,
+                orderBy: "startTime",
+            },
+            (error, response) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(response);
+                }
+            }
+        );
+    })
+    .then((results) => {
+        return {
+            statusCode: 200,
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Credentials': true,
+            },
+            body: JSON.stringify({ events: results.data.items }),
+        };
+    })
+    .catch((error) => {
+        return {
+            statusCode: 500,
+            body: JSON.stringify(error),
+        };
+    });
 };
