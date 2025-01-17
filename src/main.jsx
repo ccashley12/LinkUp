c08f21a7aa265470ca862a818df806b3c20458ae
ccashley12 <ccashley12@gmail.com>
2025-01-17 11:53:22 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+import App from './App';
 import './index.css';
 import * as atatus from 'atatus-spa';
 atatus.config('5871960039b64fedb3ef0ff032dbe96b').install();
