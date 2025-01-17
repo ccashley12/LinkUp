4f93e5ded7f84deba1033240efbed17972f3b0ae
ccashley12 <ccashley12@gmail.com>
2025-01-17 11:21:01 -0500

    installed atatus

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
+import * as atatus from 'atatus-spa';
+atatus.config('5871960039b64fedb3ef0ff032dbe96b').install();
 
 
 const root = ReactDOM.createRoot(document.getElementById('root')); 
