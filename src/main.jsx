c9ae8d9d20e6f24e6d1ef81cfc7fd13d9d4a1108
ccashley12 <ccashley12@gmail.com>
2025-01-20 13:54:36 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,10 @@ import './index.css';
 import * as atatus from 'atatus-spa';
 atatus.config('5871960039b64fedb3ef0ff032dbe96b').install();
 
-
 const root = ReactDOM.createRoot(document.getElementById('root')); 
 
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 );
