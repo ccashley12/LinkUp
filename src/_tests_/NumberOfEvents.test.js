3a42b6f45bb4f9b51251993bb5ea08f85a6e5f74
ccashley12 <ccashley12@gmail.com>
2025-01-28 13:05:12 -0500

    added code for PWA

--- src/_tests_/NumberOfEvents.test.js
+++ src/_tests_/NumberOfEvents.test.js
@@ -33,20 +33,3 @@ describe('<NumberOfEvents /> Component', () => {
         expect(input).toHaveValue('10');
     });
 });
-
-describe('NumberOfEvents /> integration', () => {
-    test('updates the number of events displayed when the user changes the number of events input', async () => {
-        const user = userEvent.setup();
-        const AppComponent = render(<App />);
-        const AppDOM = AppComponent.container.firstChild;
-
-        const NumberOfEventsDOM = AppDOM.querySelector('#number-of-events');
-        const NumberOfEventsInput = within(NumberOfEventsDOM).queryByTestId('numberOfEventsInput');
-
-        await user.type(NumberOfEventsInput, '{backspace}{backspace}10');
-
-        const EventListDOM = AppDOM.querySelector('#event-list');
-        const allRenderedEventItems = within(EventListDOM).queryAllByRole('listitem');
-        expect(allRenderedEventItems.length).toBe(10);
-    });
-});
