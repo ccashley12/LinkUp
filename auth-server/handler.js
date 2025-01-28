c29cd3eb3d926b148dc24d9f393e1874395fae9c
ccashley12 <ccashley12@gmail.com>
2025-01-28 13:34:54 -0500

    updated code format

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -4,9 +4,7 @@ const { google } = require("googleapis");
 const calendar = google.calendar("v3");
 const SCOPES = ["https://www.googleapis.com/auth/calendar.events.public.readonly"];
 const { CLIENT_SECRET, CLIENT_ID, CALENDAR_ID } = process.env;
-const redirect_uris = [
-    "https://ccashley12.github.io/meet/"
-];
+const redirect_uris = ["https://ccashley12.github.io/meet/"];
 
 const oAuth2Client = new google.auth.OAuth2(
     CLIENT_ID,
