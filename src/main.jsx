860c73010cc8367c34afb86349cf32e3ff9ab5d5
ccashley12 <ccashley12@gmail.com>
2025-01-31 11:43:23 -0500

    reformatted code

--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,10 @@ import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import * as atatus from 'atatus-spa';
 atatus.config('5871960039b64fedb3ef0ff032dbe96b').install();
 
-const root = ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
 serviceWorkerRegistration.register();
