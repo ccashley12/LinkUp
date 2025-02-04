19a0903f43e36a16ba50683b45dd33e887c59f84
ccashley12 <ccashley12@gmail.com>
2025-02-04 11:26:24 -0500

    reformatted code

--- src/components/EventList.jsx
+++ src/components/EventList.jsx
@@ -4,8 +4,9 @@ import Event from './Event';
 const EventList = ({ events }) => {
     return (
         <ul id="event-list" className="event-list-container">
-            {events? 
-                events.map((event)  => <Event key={event.id} event={event} />): null}
+            {events
+                ? events.map((event)  => <Event key={event.id} event={event} />)
+                : null}
         </ul>
     );
 }
