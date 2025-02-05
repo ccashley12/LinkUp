5a32ece43aef8c8cf2f17218040e33adb7df8637
ccashley12 <ccashley12@gmail.com>
2025-02-05 13:37:50 -0500

    updated serviceworker format

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import * as atatus from 'atatus-spa';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 atatus.config('5871960039b64fedb3ef0ff032dbe96b').install();
 
