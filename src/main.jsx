4fd9ef350f7bc8541e5c048e48c1daf0d4a1253b
ccashley12 <ccashley12@gmail.com>
2025-02-05 13:29:26 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App';
 import './index.css';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import * as atatus from 'atatus-spa';
+
 atatus.config('5871960039b64fedb3ef0ff032dbe96b').install();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
