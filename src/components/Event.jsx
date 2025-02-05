a6de56cc5fdd79b056894fd60d87ed1cf0d534e4
ccashley12 <ccashley12@gmail.com>
2025-02-05 13:26:27 -0500

    updated event component to include formatting for date & time

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -3,20 +3,38 @@ import React, { useState } from "react";
 const Event = ({ event }) => {
   const [showDetails, setShowDetails] = useState(false);
 
-  const toggleDetails = () => {
-    setShowDetails(!showDetails)
-  }
+  if (!event) {
+		return <p>No event data available</p>;
+	}
+
+	const startTime =
+		event.start && event.start.dateTime
+			? new Date(event.start.dateTime).toLocaleString()
+			: 'Start time not available';
+	const endTime =
+		event.end && event.end.dateTime
+			? new Date(event.end.dateTime).toLocaleString()
+			: 'End time not available';
 
   return (
-    <li className="eventSummary">
-        <h2>{event.summary}</h2>
-        <p>{event.location}</p>
-        <p>{event.created}</p>
-      <button className="details-btn" onClick={toggleDetails}>
-          {showDetails ? "Hide Details" : "Show Details"}
-      </button>
-      {showDetails ? <div className="eventDetails">{event.description}</div> : null}
-    </li>
+    <div className="eventContainer">
+			<div className="eventInfo">
+				<h2>{event.summary}</h2>
+				<div className="eventLess">
+					<p>Location: {event.location || 'No location provided'}</p>
+					<p>Start: {startTime}</p>
+					<p>End: {endTime}</p>
+				</div>
+			</div>
+			<button
+				data-testid={`more-btn-${event.id}`}
+				aria-label="More or Less info"
+				className="more-btn"
+				onClick={() => setShowDetails(!showDetails)}>
+				{showDetails ? 'Less info' : 'More info'}
+			</button>
+			{showDetails && <p className="eventDetails">{event.description}</p>}
+		</div>
   );
 }
   
