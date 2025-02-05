ba4a9ac6ae066c4fdf95c2155beaf574b834d91d
ccashley12 <ccashley12@gmail.com>
2025-02-05 13:23:04 -0500

    updated test for e2e

--- src/_tests_/EndToEnd.test.js
+++ src/_tests_/EndToEnd.test.js
@@ -1,33 +1,39 @@
 import puppeteer from "puppeteer";
 
-describe ('show/hide an event details', () => {
-    let browser;
-    let page;
-    beforeAll(async () => {
-        browser = await puppeteer.launch({});
-        page = await browser.newPage();
-        await page.goto('http://localhost:5173/');
-        await page.waitForSelector('.eventSummary');
-    });
+describe('show/hide an event details', () => {
+	let browser;
+	let page;
 
-    afterAll(() => {
-        browser.close();
-    });
+	beforeAll(async () => {
+		browser = await puppeteer.launch({
+			headless: false,
+			slowMo: 250,
+			timeout: 0,
+		});
 
-    test('An event element is collapsed by default.', async () => {
-        const eventDetails = await page.$('.eventSummary .eventDetails');
-        expect(eventDetails).toBeNull();
-    });
+		page = await browser.newPage();
+		await page.goto('http://localhost:5173/');
+	});
 
-    test('User can expand an event to see details.', async () => {
-        await page.click('.details-btn');
-        const eventDetails = await page.$('.eventSummary .eventDetails');
-        expect(eventDetails).toBeDefined();
-    });
+	afterAll(async () => {
+		if (browser) {
+			await browser.close();
+		}
+	});
 
-    test('User can collapse an event to hide details.', async () => {
-        await page.click('.details-btn');
-        const eventDetails = await page.$('.eventSummary .eventDetails');
-        expect(eventDetails).toBeNull();
-    });
+	it('An event element is collapsed by default', async () => {
+		const eventDetails = await page.$('.eventContainer .eventDetails');
+		expect(eventDetails).toBeNull();
+	});
+
+	it('User can expand an event to see details', async () => {
+		await page.click('.eventContainer .more-btn');
+		const eventDetails = await page.$('.eventContainer .eventDetails');
+		expect(eventDetails).toBeDefined();
+	});
+	it('User can collapse an event to hide details', async () => {
+		await page.click('.eventContainer .more-btn');
+		const eventDetails = await page.$('.eventContainer .eventDetails');
+		expect(eventDetails).toBeNull();
+	});
 });
