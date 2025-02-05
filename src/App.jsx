819b4e341c4c50c195844d70ae5b108af61de900
ccashley12 <ccashley12@gmail.com>
2025-02-05 13:25:19 -0500

    updated city search code to fix error

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useCallback } from 'react';
 import CitySearch from './components/CitySearch';
 import CityEventsChart from './components/CityEventsChart';
 import EventList from './components/EventList';
@@ -11,63 +11,105 @@ import './App.css';
 
 const App = () => {
   const [events, setEvents] = useState([]);
-  const [currentNOE, setCurrentNOE] = useState(32);
-  const [allLocations, setAllLocations] = useState([]);
-  const [currentCity, setCurrentCity] = useState("See all cities");
-  const [infoAlert, setInfoAlert] = useState("");
-  const [errorAlert, setErrorAlert] = useState("");
-  const [warningAlert, setWarningAlert] = useState("");
+	const [locations, setLocations] = useState([]);
+	const [filteredEvents, setFilteredEvents] = useState([]);
+	const [eventCount, setEventCount] = useState(32);
+	const [loading, setLoading] = useState(true);
+	const [currentCity, setCurrentCity] = useState('');
+	const [infoAlert, setInfoAlert] = useState('');
+	const [errorAlert, setErrorAlert] = useState('');
+	const [warningAlert, setWarningAlert] = useState('');
 
-  useEffect(() => {
-    let errorText;
-    if (isNaN(currentNOE)) {
-      errorText = "Please enter a valid number to see the events"
-    } else if (currentNOE < 1) {
-      errorText = "Please enter a number greater than zero to see the events"
-    } else {
-      errorText = ""
-    }
-    setErrorAlert(errorText);
-    let warningText = "";
-    if (!navigator.onLine) {
-      warningText = "Your are offline. The displayed list has been loaded from the cache";
-    }
-    setWarningAlert(warningText);
-    fetchData();
-  }, [currentCity, currentNOE, errorAlert]);
+	useEffect(() => {
+		if (navigator.onLine) {
+			setWarningAlert('');
+		} else {
+			setWarningAlert(
+				'The displayed list has been loaded from the cache. It may not be up to date.'
+			);
+		}
+		const fetchData = async () => {
+			setLoading(true);
+			const allEvents = await getEvents();
+			setEvents(allEvents);
+			const allLocations = extractLocations(allEvents);
+			setLocations(allLocations);
+			setLoading(false);
+			setFilteredEvents(allEvents.slice(0, eventCount));
+		};
+		fetchData();
+	}, [eventCount]);
 
-  const fetchData = async () => {
-    const allEvents = await getEvents();
-    const filteredEvents = currentCity === "See all cities" ?
-      allEvents :
-      allEvents.filter(event => event.location === currentCity)
-    setEvents(filteredEvents.slice(0, currentNOE));
-    setAllLocations(extractLocations(allEvents));
-  };
+	const filterEvents = useCallback(
+		(city, count) => {
+			let filtered = events;
+
+			if (city !== '') {
+				filtered = events.filter((event) =>
+					event.location.toUpperCase().includes(city.toUpperCase())
+				);
+			}
+			setFilteredEvents(filtered.slice(0, count));
+		},
+		[events]
+	);
+
+	useEffect(() => {
+		filterEvents(currentCity, eventCount);
+	}, [events, currentCity, eventCount, filterEvents]);
+
+	const handleCityChange = (city) => {
+		setCurrentCity(city);
+		filterEvents(city, eventCount);
+
+		let filtered = events;
+		if (city !== '') {
+			filtered = events.filter((event) =>
+				event.location.toUpperCase().includes(city.toUpperCase())
+			);
+		}
+		setFilteredEvents(filtered.slice(0, eventCount));
+	};
+
+	const handleNumberOfEventsChange = (number) => {
+		const num = parseInt(number, 10);
+		setEventCount(num);
+
+		let filtered = events;
+		if (currentCity !== '') {
+			filtered = events.filter((event) =>
+				event.location.toUpperCase().includes(currentCity.toUpperCase())
+			);
+		}
+		setFilteredEvents(filtered.slice(0, num));
+	};
 
     return (
         <div className="App">
+          {loading && <p className="loading">Loading...</p>}
           <div className="alerts-container">
             {infoAlert.length ? <InfoAlert text={infoAlert}/> : null}
             {errorAlert.length ? <ErrorAlert text={errorAlert}/> : null}
             {warningAlert.length ? <WarningAlert text={warningAlert}/> : null}
           </div>
+          <div className="filter-container">
             <CitySearch 
-              allLocations={allLocations} 
-              setCurrentCity={setCurrentCity}
+              allLocations={locations} 
+              onCityChange={handleCityChange}
               setInfoAlert={setInfoAlert}
             />
             <NumberOfEvents
-              currentNOE={currentNOE}
-              setCurrentNOE={setCurrentNOE}
+              currentEventCount={eventCount}
+              setCurrentNOE={handleNumberOfEventsChange}
               setErrorAlert={setErrorAlert}
             />
+          </div>
             <div className="charts-container">
-              <EventGenresChart events={events} />
-              <CityEventsChart allLocations={allLocations} events={events} />
+              <EventGenresChart events={filteredEvents} />
+              <CityEventsChart allLocations={locations} events={filteredEvents} />
             </div>
             <EventList 
-              events={events}
+              events={filteredEvents}
             />
         </div>
     );
