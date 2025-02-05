da3f46be08aa0e34d6770df1c7a0b8d88a83cb43
ccashley12 <ccashley12@gmail.com>
2025-02-05 13:25:54 -0500

    updated citysearch code functions to correct error

--- src/components/CitySearch.jsx
+++ src/components/CitySearch.jsx
@@ -1,65 +1,92 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
 
-const CitySearch = ({ allLocations, setCurrentCity, setInfoAlert }) => {
+const CitySearch = ({ allLocations, onCityChange, setInfoAlert }) => {
     const [showSuggestions, setShowSuggestions] = useState(false);
     const [query, setQuery] = useState("");
     const [suggestions, setSuggestions] = useState([]);
 
-    useEffect(() => {
-        setSuggestions(allLocations);
-    }, [`${allLocations}`]);
+    const locations = useMemo(() => 
+        (Array.isArray(allLocations) ? allLocations : []),
+        [allLocations]
+    );
     
     const handleInputChanged = (event) => {
         const value = event.target.value;
-        const filteredLocations = allLocations ? allLocations.filter((location) => {
-            return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
-        }) : [];
+        let filteredLocations = [];
+        let infoText = '';
+
+        if (value === '') {
+            filteredLocations = [];
+        } else {
+            filteredLocations = allLocations.filter((location) => 
+                location.toUpperCase().includes(value.toUpperCase())
+            );
+        }
+        if (value && filteredLocations.length === 0) {
+            infoText = 'We cannot locate the city you are trying to find. Please try a different city.';
+        }
+        if (value === '') {
+            infoText = '';
+        }
 
         setQuery(value);
         setSuggestions(filteredLocations);
-
-        let infoText;
-        if (filteredLocations.length === 0) {
-            infoText = "Unable to locate city. Please try another city."
-        } else {
-            infoText = ""
-        }
         setInfoAlert(infoText);
+        onCityChange(value);
     };
 
     const handleItemClicked = (event) => {
         const value = event.target.textContent;
-        setQuery(value);
+        setQuery(value === 'See all cities' ? '' : value);
         setShowSuggestions(false);
-        setCurrentCity(value);
-        setInfoAlert("");
+        onCityChange(value === 'See all cities' ? '' : value);
+        setInfoAlert('');
     };
 
+    useEffect(() => {
+		if (locations && locations.length > 0) {
+			setSuggestions(locations);
+		}
+	}, [locations]);
+
     return (
-        <div id="city-search">
-            <input
-                type="text"
-                className="city"
-                placeholder="Search for City"
-                value={query}
-                onFocus={() => setShowSuggestions(true)}
-                onChange={handleInputChanged}
-            />
-            {showSuggestions ? 
-                <ul className="suggestions">
-                    {suggestions.map((suggestion) => {
-                        return <li 
-                                    onClick={handleItemClicked} 
-                                    key={suggestion}>{suggestion}
-                                </li>
-                    })}
-                    <li key='See all cities' 
-                        onClick={handleItemClicked}
-                    >
-                        <b>See all cities</b>
-                    </li>
-                </ul> : null}
-        </div>
+        <div
+			id="city-search"
+			className="filter-item">
+			<label
+				htmlFor="city">
+				Search for a city
+			</label>
+			<div className="inputAndSubmit">
+				<input
+					type="text"
+					className="city"
+					placeholder="Start typing ..."
+					value={query}
+					onFocus={() => setShowSuggestions(true)}
+					onChange={handleInputChanged}
+					aria-label="Search for a city"
+				/>
+				{showSuggestions && (
+					<ul className="suggestions">
+						{suggestions.map((suggestion) => (
+							<li
+								onClick={handleItemClicked}
+								key={suggestion}
+								className="suggestion-item">
+								{suggestion}
+							</li>
+						))}
+						<li
+							key="See all cities"
+							onClick={handleItemClicked}
+							className="seeAllCities">
+							<strong>See all cities</strong>
+						</li>
+					</ul>
+				)}
+			</div>
+		</div>
     );
 }
 
