356b9a81635046ad816c00abdd94ee38b3bf6617
ccashley12 <ccashley12@gmail.com>
2025-02-05 13:23:34 -0500

    updated test for eventlist component

--- src/_tests_/EventList.test.js
+++ src/_tests_/EventList.test.js
@@ -5,29 +5,36 @@ import { getEvents } from '../api';
 import App from '../App';
 
 describe('<EventList /> component', () => {
-  let EventListComponent;
-  beforeEach(() => {
-    EventListComponent = render(<EventList />);
-  })
-  test('has an element with "list" role', () => {
-    expect(EventListComponent.queryByRole("list")).toBeInTheDocument();
-  });
-  
-  test('renders correct number of events', async () => {
-    const allEvents = await getEvents(); 
-    EventListComponent.rerender(<EventList events={allEvents} />);
-    expect(EventListComponent.getAllByRole("listitem")).toHaveLength(allEvents.length);
-  });
+	let EventListComponent;
+
+	beforeEach(async () => {
+		const allEvents = await getEvents();
+
+		EventListComponent = render(<EventList events={allEvents} />);
+	});
+
+	test('has an element with "list" role', () => {
+		expect(EventListComponent.queryByRole('list')).toBeInTheDocument(); //
+	});
+
+	test('renders correct number of events', async () => {
+		const allEvents = await getEvents();
+
+		EventListComponent.rerender(<EventList events={allEvents} />);
+		const listItems = EventListComponent.getAllByRole('listitem');
+
+		expect(listItems).toHaveLength(allEvents.length);
+	});
 });
 
 describe('<EventList /> integration', () => {
-    test('renders a list of 32 events when the app is mounted and rendered', async () => {
-        const AppComponent = render(<App />);
-        const AppDOM = AppComponent.container.firstChild;
-        const EventListDOM = AppDOM.querySelector('#event-list');
-        await waitFor(() => {
-            const EventListItems = within(EventListDOM).queryAllByRole('listitem');
-            expect(EventListItems.length).toBe(32);
-        });
-    });
+	test('renders a list of 32 events when the app is mounted and rendered', async () => {
+		const AppComponent = render(<App />);
+		const AppDOM = AppComponent.container.firstChild;
+		const EventListDOM = AppDOM.querySelector('#event-list');
+		await waitFor(() => {
+			const EventListItems = within(EventListDOM).queryAllByRole('listitem');
+			expect(EventListItems).toHaveLength(32);
+		});
+	});
 });
