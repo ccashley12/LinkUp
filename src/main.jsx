ab11ea33943e3dc5c7e8e62ff9243f59db4d40f3
ccashley12 <ccashley12@gmail.com>
2025-02-06 13:19:44 -0500

    deleted unnecessary doc title

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+import App from './App';
 import './index.css';
 import * as atatus from 'atatus-spa';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
